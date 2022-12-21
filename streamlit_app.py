2dc69f538ab6189109a84c7089eda5ec08a23511
Mr. CHARANKUMAR  .N <121157345+charanreddynr@users.noreply.github.com>
2022-12-21 14:15:03 +0530

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit
 streamlit.title('my parents new healty dinner')
-streamlit.title('Breakfast Menu')
-streamlit.title('Idly, dosa')
+streamlit.header('Breakfast Menu')
+streamlit.text('Idly, dosa')
