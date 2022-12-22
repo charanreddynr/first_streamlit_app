c7c8f1b2568aceb8b8db11a266cb52dca2fc6eba
Mr. CHARANKUMAR  .N <121157345+charanreddynr@users.noreply.github.com>
2022-12-22 23:36:56 +0530

    Update streamlit_app.py
    code

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,28 +33,3 @@ streamlit.write('The user entered ', fruit_choice)
 
 #don't run anything past here while we troubleshoot
 streamlit.stop ()
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-
-# Takes the jason version of response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output as seen by table
-streamlit.dataframe(fruityvice_normalized)
-
-
-
-import snowflake. connector
-
-my_cnx = snowflake. connector.connect(**streamlit.secrets ["snowflake"])
-my_cur = my_cnx. cursor ()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur. fetchall()
-streamlit. header ("The fruit load list contains:") streamlit.dataframe (my_data_rows)
-
-
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit') 
-streamlit.write('Thanks for adding ', add_my_fruit)
-
-#This will not work correctly, but just go with it for now 
-my_cur.execute("insert into fruit load list values ('from streamlit')")
